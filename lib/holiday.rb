ec4b76b11081f8dba2d48772e9d761521654de50
Chad Montoya <chadmontoya21@yahoo.com>
2017-11-04 03:45:25 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,9 +61,9 @@ end
 
 
 def all_holidays_with_bbq(holiday_hash)
-  holiday_hash.each do |season, holiday|
-    holiday.each do |day, array|
+  holiday_hash.map do |season, holiday|
+    holiday.map do |day, array|
     day  if array.include?("BBQ")
     end
-  end
+  end.flatten.compact
 end
